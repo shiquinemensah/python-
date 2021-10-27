5d7e3825274336d92475a5b1b193700021006eba
shiquine <godchildshiquine@gmail.com>
2021-10-27 06:22:41 -0700

    edf

---
+++ index.py
@@ -0,0 +1,25 @@
+#Loops and for Loops
+shoes = ['jordans', 'nike', 'adidas']
+for x in shoes:
+    print (x)
+    if x == 'banana':
+        continue
+    print(x)
+
+
+
+jordans = ['4s' '1s' '8s']
+for x in fruits:
+    print (x)
+
+
+
+cheese = ['chedder' 'white' 'stinky' 'browncheese']
+    for x in cheese
+        print (x)
+        if x == 'chedder':
+            continue
+        print(x)
+
+# double equals (==) allows the system to compare the items
+
